1b0def6161d3ce07ee30ea4f93246bbe68869b92
Patrick Flynn <patrickf4664@protonmail.com>
2020-11-12 14:17:07 -0500

    Added overall structure and comments to describe the algorithm

--- main.cpp
+++ main.cpp
@@ -13,30 +13,24 @@ Date: 12/01/20
 #include <cmath>
 using namespace std;
 
-int randomDice1(){
-    int dice1 = (rand() % 6) + 1; 
-    return dice1;
-}
-
-int randomDice2(){
-    int dice2 = (rand() % 6) + 1; 
-    return dice2;
-}
+// Forward declaration of the game functions
+void gameLoop();
+int playerTurn(int player);
 
 int main(){
     
     srand(time(0));
     //Initialize Variables
-    int diceRoll1 = randomDice1();
-    int diceRoll2 = randomDice2();
     char instructionsNeeded; 
     
+    // Welcome the user
     cout << "\033[01;44;41m" << "Welcome to Pig Dice!" << "\x1B[0m" << endl;
+    
     //Ask User If they Need Instructions
     cout << "Would you like to see the instructions for the Pig Dice game? (y/n)";
     cin >>instructionsNeeded;
     
-    if(instructionsNeeded == 'y'){
+    if (instructionsNeeded == 'y') {
         //Output Instructions
         cout<<" Instructions:"<<endl;
         cout<<" The rules are simple: Two players race to reach 100 points. Each turn,"<<endl;
@@ -52,6 +46,39 @@ int main(){
         cout<<" becomes the opponent's turn."<<endl;
     }
     
-    cout << "Dice Roll:" << "\nDice1: " << diceRoll1 << "\nDice2: " << diceRoll2 << endl;
+    gameLoop();
     return 0;
 }
+
+// The main game loop
+// This will keep track of whose turn it is and the scores
+//
+// The algorithm:
+//   Set the current player; call player turn
+//   When playerTurn returns, the score is added to the player's current score
+//   Check the scores- if one is >= 100 then we output the winner
+//   If there is no winner, switch players and start again
+void gameLoop() {
+    
+}
+
+// We only need one function- it will generate a new random number
+// each time we call it
+int randomDice(){
+    int dice1 = (rand() % 6) + 1; 
+    return dice1;
+}
+
+
+// A player loop. This will return how much new score the user has won
+//
+// The algorithm (use do-while)
+//   Roll dice. If dice is 1, score is set to 0 and we return
+//   If not 1, add that to the user's score.
+//   Ask if they want to hold or roll again.
+//      If hold, return the score
+//      If roll again, start the loop over
+int playerTurn(int player) {
+    return 0;
+}
+

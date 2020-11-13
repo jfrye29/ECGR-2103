984f3ff2e5e953278d873ab9a15103f5bd52027d
Patrick Flynn <patrickf4664@protonmail.com>
2020-11-13 13:32:27 -0500

    Merge AI and two player version

--- main_pdf.cpp
+++ main_pdf.cpp
@@ -11,11 +11,16 @@ Date: 12/01/20
 #include <cstdlib>
 #include <ctime>  
 #include <cmath>
+#include <vector>
 using namespace std;
+
+// Set this to set the maximum (winning) score
+#define WIN_SCORE 100
 
 // Forward declaration of the game functions
 void gameLoop();
 int playerTurn();
+int computerPlayerTurn();
 
 int main(){
     
@@ -62,8 +67,16 @@ void gameLoop() {
     int currentPlayer = 1;
     int player1Score = 0;
     int player2Score = 0;
+    bool computerPlayer = false;
     
-    while (player1Score < 100 && player2Score < 100) {
+    cout << "Do you wish to fight the computer? (y,n)" << endl;
+    char choice;
+    cin >> choice;
+    if (choice == 'y' || choice == 'Y') {
+        computerPlayer = true;
+    }
+    
+    while (player1Score < WIN_SCORE && player2Score < WIN_SCORE) {
         cout << endl << "=================================" << endl;
         cout << "Player " << currentPlayer << "'s turn." << endl;
         
@@ -71,20 +84,22 @@ void gameLoop() {
         if (currentPlayer == 1) cout << player1Score;
         else cout << player2Score;
         cout << endl << endl;
-    
-        int score = playerTurn();
         
         if (currentPlayer == 1) {
-            player1Score += score;
+            player1Score += playerTurn();
             currentPlayer = 2;
         } else {
-            player2Score += score;
+            if (computerPlayer) {
+                player2Score += computerPlayerTurn();
+            } else {
+                player2Score += playerTurn();
+            }
             currentPlayer = 1;
         }
     }
     
     cout << endl;
-    if (player1Score >= 100) {
+    if (player1Score >= WIN_SCORE) {
         cout << "Player 1 won!!!!" << endl;
     } else {
         cout << "Player 2 won!!!!" << endl;
@@ -128,6 +143,74 @@ int playerTurn() {
         }
     } while (dice != 1 && !(choice == 'y' || choice == 'y'));
     
+    return score;
+}
+
+// Logic for the computer player
+// This is the idea behind the computer player:
+//
+// So we have a vector that holds all the scores either when the computer decided to hold,
+// or just before their score was zeroed after rolling 1.
+//
+// If the computer has no scores, we hold if the score is greater than 15 (because you will very likely roll
+// a 1 sooner or later).
+//
+// Otherwise, we just average all the scores. Then we check the difference between the average score and the
+// current score (taking the absolute value so we don't end up with a negative number). This value is our
+// margin. If the margin is greater than a certain threshold, then we continue. Otherwise, we hold.
+
+vector<int> lastScores;
+
+bool shouldIHold(int current) {
+    if (lastScores.size() == 0) {
+        if (current >= 15) {
+            return true;
+        }
+        return false;
+    }
+    
+    int size = lastScores.size();
+    int average = 0;
+    
+    for (int score : lastScores) {
+        average += score;   
+    }
+    
+    average = average / size;
+    int margin = abs(average-current);
+    
+    if (margin > 5) {
+        return false;
+    }
+    return true;
+}
+
+int computerPlayerTurn() {
+    int dice = 0;
+    int score = 0;
+    char choice = 0;
+    
+    do {
+        dice = randomDice();
+        cout << "Dice: " << dice << endl;
+        
+        if (dice == 1) {
+            lastScores.push_back(score);
+            
+            cout << "Score: 0. Computer's turn is over." << endl;
+            return 0;
+        } else {
+            score += dice;
+            
+            if (!shouldIHold(score)) {
+                choice = 'y';
+            }
+        }
+    } while (dice != 1 && !(choice == 'y' || choice == 'y'));
+    
+    cout << "Computer holding at: " << score << endl;
+    
+    lastScores.push_back(score);
     return score;
 }
 
